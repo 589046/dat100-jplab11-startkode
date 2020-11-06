96461596abaf2d46530383f0cb1dfc955e6315ef
Snadderbyte <69865773+Snadderbyte@users.noreply.github.com>
2020-11-06 15:02:09 +0100

    Ferdig med alt

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,46 +5,71 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] tabell;
+	private int ledig;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		ledig = 0;
+		tabell = new Innlegg[20];
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		ledig = 0;
+		tabell = new Innlegg[lengde];
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return ledig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return tabell;
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		for (int i = 0; i < ledig; i++) {
+			if (tabell[i].erLik(innlegg)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		for (Innlegg i : tabell) {
+			if (i != null && i.erLik(innlegg) ) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		 return ledig < tabell.length;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		if (ledigPlass() && !finnes(innlegg)) {
+			 tabell[ledig] = innlegg;
+			 ledig++;
+			 
+			 return true;
+		 } else {
+			 return false;
+		 }
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String str = ledig + "\n";
+		
+		for (Innlegg i : tabell) {
+			str += i.toString();
+		}
+		
+		return str;
 	}
 
 	// valgfrie oppgaver nedenfor
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -9,7 +9,22 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter print = null;
+	
+        try {
+            print = new PrintWriter(mappe + filnavn);
+            String str = samling.toString();
+            print.write(str);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Filen finnst ikkje!");
+        }
+        finally {
+            if (print != null) {
+                print.close();
+                return true; 
+            }
+        }
+        return false;
 	}
 }
